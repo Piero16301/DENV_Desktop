791be7c2411f0e65a8c4001756af1d0931b596f2
Piero Angelo Morales Alcalde <piero.morales@utec.edu.pe>
2022-04-25 14:20:49 -0500

    Se agrega paginas iniciales

---
+++ lib/sources/pages/data_table_page.dart
@@ -0,0 +1,14 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+class DataTablePage extends StatelessWidget {
+  const DataTablePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const ScaffoldPage(
+      content: Center(
+        child: Text('Data Table Page'),
+      ),
+    );
+  }
+}
---
+++ lib/sources/pages/pages.dart
@@ -0,0 +1,3 @@
+export 'package:deteccion_zonas_dengue_desktop/sources/pages/home_page.dart';
+export 'package:deteccion_zonas_dengue_desktop/sources/pages/data_table_page.dart';
+export 'package:deteccion_zonas_dengue_desktop/sources/pages/settings_page.dart';
